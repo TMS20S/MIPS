b6eb3af93ee8b744c7da7a9a8bc581c53106e88c
TMS20S <93251056+TMS20S@users.noreply.github.com>
2023-11-09 14:02:07 +0200

    tms

--- mips/loop.asm
+++ mips/loop.asm
@@ -1,7 +1,22 @@
 .data
  w: .word 9 2 5
- D: .word 
+ N: .asciiz "Name: "
+ D: .space 100 
+ V: .word 100
 .text
 .globl main
 main:
     
+li $v0, 8
+ la $a0, D
+ lw $a1,V 
+ syscall
+ 
+ li $v0,4
+ la $a0,N
+ syscall
+ 
+ 
+ li $v0,4
+ la $a0,D
+ syscall
